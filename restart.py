f03505058261017a1e486d2bec686c33c61fdf1a
김경빈 <bill_server@gimgyeongbins-Mac-mini.local>
2022-02-18 17:14:16 +0900

    interval chagne

--- restart.py
+++ restart.py
@@ -9,4 +9,4 @@ while True:
         os.system("pip3 install ccxt")
         os.system("pip3 install --upgrade ta")
         requests.post("https://slack.com/api/chat.postMessage", headers={"Authorization": "Bearer " + 'xoxb-2099767973728-2069221524710-7gx1Hx2jEzlyV03HEuVKCWsc'}, data={"channel": '#bitcoin', "text": 'Starting........'})
-        os.system("python3 /root/code_anywhere/BITCOIN/Stocastic_RSI_MACD.py")
+        os.system("python3 /Users/bill_server/Desktop/BITCOIN/Stocastic_RSI_MACD.py")
